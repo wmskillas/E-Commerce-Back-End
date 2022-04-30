b12d8a1fb5d093a0402440141c0993295a3bb6c6
William Skillas <wmskillas@gmail.com>
2022-04-30 01:06:20 -0400

    began readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # E-Commerce-Back-End
+This app allows you to access, add, or delete from the db information.
+
+## gitHub
+[gitHub Repo](https://github.com/wmskillas/E-Commerce-Back-End)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,7 +111,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: { id: req.params.id }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete one product by its `id` value
 });
 
